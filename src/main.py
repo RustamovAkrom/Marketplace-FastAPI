2b406a0544938c52346db481e7ab4deb2c15208b
= <akromjonrustamov56@gmail.com>
2025-12-06 20:38:29 +0500

    fix: fix test error

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from core.config import settings
 if __name__ == "__main__":
     uvicorn.run(
         "app:create_app",
-        app_dir="src",
         host=settings.SERVER_HOST,
         port=settings.SERVER_PORT,
         reload=settings.RELOAD,
