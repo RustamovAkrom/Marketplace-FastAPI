8e59668735c91dffead64066d92e56e22c9de330
= <akromjonrustamov56@gmail.com>
2025-12-06 21:30:17 +0500

    full commit

--- src/core/email.py
+++ src/core/email.py
@@ -3,19 +3,17 @@ from fastapi_mail import ConnectionConfig, FastMail
 
 from core.config import settings
 
-# Валидация, чтобы не было None
-if settings.ENV == "prod":
-    conf = ConnectionConfig(
-        MAIL_USERNAME=settings.SMTP_USER,
-        MAIL_PASSWORD=settings.SMTP_PASSWORD,
-        MAIL_FROM=settings.EMAIL_FROM,
-        MAIL_PORT=settings.SMTP_PORT,
-        MAIL_SERVER=settings.SMTP_HOST,
-        MAIL_STARTTLS=settings.SMTP_TLS,
-        MAIL_SSL_TLS=not settings.SMTP_TLS,
-        USE_CREDENTIALS=True,
-        VALIDATE_CERTS=True,
-        # TEMPLATE_FOLDER="templates/email",  # если используешь шаблоны
-    )
+conf = ConnectionConfig(
+    MAIL_USERNAME=settings.SMTP_USER,
+    MAIL_PASSWORD=settings.SMTP_PASSWORD,
+    MAIL_FROM=settings.EMAIL_FROM,
+    MAIL_PORT=settings.SMTP_PORT,
+    MAIL_SERVER=settings.SMTP_HOST,
+    MAIL_STARTTLS=settings.SMTP_TLS,
+    MAIL_SSL_TLS=not settings.SMTP_TLS,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    # TEMPLATE_FOLDER="templates/email",  # если используешь шаблоны
+)
 
-    fast_mail = FastMail(conf)
+fast_mail = FastMail(conf)
